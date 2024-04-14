448da13321e0787e8ec793c5d2939688b6d4fa7c
Nikhil Chaudhary <Nikhil.ace.chd@gmail.com>
2024-04-14 22:19:23 +0530

    Update main.js

--- main.js
+++ main.js
@@ -46,18 +46,18 @@ function chooseCard(total=0, turn=1){
 
 function addCard(player, card){
     if(player.id === "dealer-cards"){
-        let command = '<img class="card" src="/cards/' + card + ".png" +  '">'
+        let command = '<img class="card" src="/NikksBlackjack/cards/' + card + ".png" +  '">'
         if(player.childNodes.length === 0){
-            player.innerHTML = command + '<img class="card" src="/cards/hidden.png">'
+            player.innerHTML = command + '<img class="card" src="/NikksBlackjack/cards/hidden.png">'
         }
         dealerString += command
     }
     else{
         if(player.childNodes.length <= 1){
-            let command = '<img class="card" src="/cards/' + card + ".png" +  '">'
+            let command = '<img class="card" src="/NikksBlackjack/cards/' + card + ".png" +  '">'
             player.innerHTML += command
         }else{
-            let command = '<img id="playerCard" class="hidden card" src="/cards/' + card + ".png" +  '">'
+            let command = '<img id="playerCard" class="hidden card" src="/NikksBlackjack/cards/' + card + ".png" +  '">'
             player.innerHTML += command
         }
     }   
